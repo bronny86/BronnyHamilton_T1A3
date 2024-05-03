13daf9180b32f2c8a40fa58199e0901a76fd1d9c
bronny86 <bronnyhamilton@gmail.com>
2024-05-03 10:12:09 +1000

    added text.txt

--- wordle.py
+++ wordle.py
@@ -9,6 +9,11 @@ def print_menu():
     print("Let's play Wordle!")
     print("Six chances to guess a five letter word. Type your first guess and then hit enter!\n")
 
+def read_random_word():
+    with open ("words.txt") as f:
+        words = f.read().splitlines()
+        return random.choice(words).lower()
+
 nltk.data.path.append('/work/words')
 word_list = words.words()
 words_five = [word for word in word_list if len(word) == 5]
