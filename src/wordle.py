73e0f1c4746c7b2b477ff6607a45fed9ad0d8ae7
bronny86 <bronnyhamilton@gmail.com>
2024-05-03 23:38:22 +1000

    added autopep to fix style

--- src/wordle.py
+++ src/wordle.py
@@ -1,42 +1,45 @@
+from nltk.corpus import words
 import random
 import sys
 from termcolor import colored
 import nltk
 nltk.download('words')
-from nltk.corpus import words
+
 
 def print_menu():
     print("Let's play Wordle!")
     print("Six chances to guess a five letter word. Type your first guess and then hit enter!\n")
 
+
 def read_random_word():
-    with open ("words.txt") as f:
+    with open("words.txt") as f:
         words = f.read().splitlines()
         return random.choice(words).lower()
-    
-def eval_attempt(a, s): #a = attempt s = secret word
-    res = [' '] * 5 #result as list of chars
-    #'''encoding for list elements:
-    #'*' - char. is correctly placed
-    #'+' - char. is in secret word but wrong place
-    #'_' char. is not in the secret word
-    #'''
 
-    s2 = list(s) # copt of s as list of chars
-    a2 = list(a) # copy of a as list of chars
+
+def eval_attempt(a, s):  # a = attempt s = secret word
+    res = [' '] * 5  # result as list of chars
+    # '''encoding for list elements:
+    # '*' - char. is correctly placed
+    # '+' - char. is in secret word but wrong place
+    # '_' char. is not in the secret word
+    # '''
+
+    s2 = list(s)  # copt of s as list of chars
+    a2 = list(a)  # copy of a as list of chars
     # mark '*' chars
     for pos, char in enumerate(a):
         if char == s[pos]:
             res[pos] = '*'
-            a2[pos] = '0' #mark char as 'checked'
-            s2.remove(char) #remove from s2
+            a2[pos] = '0'  # mark char as 'checked'
+            s2.remove(char)  # remove from s2
         # mark '+', '_' in chars
     for pos, char in enumerate(a2):
-        if char != '0': #only check chars that are not already checked
+        if char != '0':  # only check chars that are not already checked
             if char in s2:
                 res[pos] = '+'
                 a2[pos] = '0'
-                s2.remove(char) #ermove from s2
+                s2.remove(char)  # ermove from s2
             else:
                 res[pos] = '_'
         # print the attempt as colored chars
@@ -45,7 +48,7 @@ def eval_attempt(a, s): #a = attempt s = secret word
             print(char, end='')
         elif res[pos] == '+':
             print(colored(char, 'yellow'), end="")
-        else: #'*'
+        else:  # '*'
             print(colored(char, 'green'), end="")
 
 
@@ -57,11 +60,11 @@ print(len(words_five))
 print_menu()
 play_again = " "
 while play_again != "q":
-    word = random.choice(words_five).lower() #using NLTK
+    word = random.choice(words_five).lower()  # using NLTK
     # word = read_random_word() using file words.txt for testing
     for attempt in range(1, 7):
         guess = input().lower()
-        #input validation
+        # input validation
         while len(guess) != 5 or not guess.isalpha():
             print("Incorrect input. Try Again!")
             guess = input().lower()
@@ -78,5 +81,3 @@ while play_again != "q":
         elif attempt == 6:
             print(f"Sorry the wordle was.. {word}")
     play_again = input("Want to play again? Type q to exit")
-
-
