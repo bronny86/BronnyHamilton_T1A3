1dbd84bdf6fd022929cb5e444fbb7be9697cd7e7
bronny86 <bronnyhamilton@gmail.com>
2024-05-03 23:50:53 +1000

    fixing code to fit style guide

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # T1A3 Python Terminal App - Wordle Clone
 
-- six turns to guess five letter word. 
+- six turns to guess five letter word.
 
 GitHub Repo Link
 
 Trello board:
-
--- src/wordle.py
+++ src/wordle.py
@@ -1,21 +1,18 @@
-from nltk.corpus import words
 import random
 import sys
+from nltk.corpus import words
 from termcolor import colored
 import nltk
 nltk.download('words')
-
 
 def print_menu():
     print("Let's play Wordle!")
     print("Six chances to guess a five letter word. Type your first guess and then hit enter!\n")
 
-
 def read_random_word():
     with open("words.txt") as f:
         words = f.read().splitlines()
         return random.choice(words).lower()
-
 
 def eval_attempt(a, s):  # a = attempt s = secret word
     res = [' '] * 5  # result as list of chars
@@ -58,8 +55,8 @@ words_five = [word for word in word_list if len(word) == 5]
 print(len(words_five))
 
 print_menu()
-play_again = " "
-while play_again != "q":
+PLAY_AGAIN = " "
+while PLAY_AGAIN != "q":
     word = random.choice(words_five).lower()  # using NLTK
     # word = read_random_word() using file words.txt for testing
     for attempt in range(1, 7):
@@ -80,4 +77,4 @@ while play_again != "q":
             break
         elif attempt == 6:
             print(f"Sorry the wordle was.. {word}")
-    play_again = input("Want to play again? Type q to exit")
+    PLAY_AGAIN = input("Want to play again? If YES type a new five letter word. If NO type q to exit")
